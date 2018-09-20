380136841913c5e5440a9a49378aa2979ae115df
Yobol <m15152036579@163.com>
2018-09-20 23:50:57 +0800

    Merge two sorted lists

---
+++ src/me/yobol/yoalgo/linkedlist/MergeTwoSortedLists.java
@@ -0,0 +1,18 @@
+package me.yobol.yoalgo.linkedlist;
+
+import me.yobol.yoalgo.basic.ListNode;
+
+/**
+ * Description:
+ * Merge two sorted linked lists and return it as a new list. The new list should be made by splicing together the nodes of the first two lists.
+ *
+ * Example:
+ * Input: 1->2->4, 1->3->4
+ * Output: 1->1->2->3->4->4
+ */
+public class MergeTwoSortedLists {
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2){
+        return null;
+    }
+}
