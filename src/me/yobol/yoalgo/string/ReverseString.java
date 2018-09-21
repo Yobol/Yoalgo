c24635b024662342ee56d64cc887d41b1d24962b
Yobol <m15152036579@163.com>
2018-09-22 00:56:20 +0800

    Reverse String

--- src/me/yobol/yoalgo/string/ReverseString.java
+++ src/me/yobol/yoalgo/string/ReverseString.java
@@ -2,13 +2,39 @@ package me.yobol.yoalgo.string;
 
 public class ReverseString {
 
+    private String reverseString1(String s) {
+        // StringBuilder
+        //return new StringBuilder(s).reverse().toString();
+        StringBuilder sb = new StringBuilder(s);
+        int i;
+        for (i = s.length() - 1;i >= 0;i--){
+            sb.append(s.charAt(i));
+        }
+        return sb.substring(s.length());
+    }
 
-    public String reverseString(String s) {
-        return new StringBuilder(s).reverse().toString();
+    /**
+     * Two Pointers
+     *
+     * @param s
+     * @return
+     */
+    private String reverseString2(String s) {
+        if (s == null) return null;
+        int i = 0,j = s.length() - 1;
+        char[] cs = s.toCharArray();
+        while(i < j){
+            char temp = cs[i];
+            cs[i] = cs[j];
+            cs[j] = temp;
+            i++;
+            j--;
+        }
+        return new String(cs);
     }
 
     public static void main(String[] args) {
         ReverseString rs = new ReverseString();
-        System.out.print(rs.reverseString("Hello"));
+        System.out.print(rs.reverseString2("Hello"));
     }
 }
