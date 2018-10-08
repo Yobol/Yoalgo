f8fa5fdd8e1df2e42bc7c44d2bfe015c9f0ad269
Yobol <m15152036579@163.com>
2018-10-08 23:49:43 +0800

    Merge Intervals

--- src/me/yobol/yoalgo/sort/MergeIntervals.java
+++ src/me/yobol/yoalgo/sort/MergeIntervals.java
@@ -2,8 +2,7 @@ package me.yobol.yoalgo.sort;
 
 import me.yobol.yoalgo.basic.Interval;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 /**
  * Description:
@@ -21,39 +20,40 @@ import java.util.List;
  */
 public class MergeIntervals {
 
+    private class IntervalComparator implements Comparator<Interval> {
+        @Override
+        public int compare(Interval a, Interval b) {
+            return a.start < b.start ? -1 : a.start == b.start ? 0 : 1;
+        }
+    }
+
     public List<Interval> merge(List<Interval> intervals) {
-        if (intervals == null) return null;
-        if (intervals.size() <= 1) return intervals;
+        Collections.sort(intervals, new IntervalComparator());
 
-        List<Interval> result = new ArrayList<>();
-        Interval current, prior = null;
-
-        // 一次将原Interval列表中的元素取出
-        while (!intervals.isEmpty()) {
-            current = intervals.remove(0);
-
-            if (prior != null) {
-                if (prior.end >= current.start) {
-                    Interval temp = new Interval(prior.start, current.end);
-                    result.add(temp);
-                    prior = temp;
-                } else {
-                    result.add(current);
-                }
-            } else {
-                prior = current;
+        LinkedList<Interval> merged = new LinkedList<Interval>();
+        for (Interval interval : intervals) {
+            // if the list of merged intervals is empty or if the current
+            // interval does not overlap with the previous, simply append it.
+            if (merged.isEmpty() || merged.getLast().end < interval.start) {
+                merged.add(interval);
+            }
+            // otherwise, there is overlap, so we merge the current and previous
+            // intervals.
+            else {
+                merged.getLast().end = Math.max(merged.getLast().end, interval.end);
             }
         }
-        return result;
+
+        return merged;
     }
 
     public static void main(String[] args) {
         MergeIntervals mi = new MergeIntervals();
         List<Interval> intervals = new ArrayList<>();
         intervals.add(new Interval(1, 3));
-//        intervals.add(new Interval(2, 6));
-//        intervals.add(new Interval(8, 10));
-//        intervals.add(new Interval(15, 18));
+        intervals.add(new Interval(0, 6));
+        intervals.add(new Interval(8, 10));
+        intervals.add(new Interval(15, 18));
         for (Interval i : intervals) {
             System.out.print("[" + i.start + ", " + i.end + "]  ");
         }
