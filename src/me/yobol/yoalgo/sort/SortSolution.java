dd52ea58c461197998117a7c9e29a42925fbb02a
Yobol <m15152036579@163.com>
2019-01-06 22:35:21 +0800

    Insertion Sort

--- src/me/yobol/yoalgo/sort/SortSolution.java
+++ src/me/yobol/yoalgo/sort/SortSolution.java
@@ -24,24 +24,37 @@ public class SortSolution {
     }
 
     /**
+     * 每次都将一个待排序的元素，按其关键字大小插入到前面已经排好序的子序列中，直到全部元素插入完成
+     * @param a
+     */
+    private static void insertionSort(int[] a) {
+        int j;
+
+        for (int i = 1; i < a.length; i++) {
+            int cur = a[i];
+
+            for (j = i; j >= 1 && cur < a[j - 1]; j--) {
+                a[j] = a[j - 1];
+            }
+            a[j] = cur;
+        }
+    }
+
+    /**
      * 每次都从待排序的元素中选出最小/大的元素，放到已排序元素的后面
      * select the smallest/biggest element from the elements to be sorted every time，and put it after the sorted elements
-     * @param a
-     * @return
      */
-    public static int[] selectionSort(int[] a) {
+    private static void selectionSort(int[] a) {
         for (int i = 0;i < a.length;i++) {
             int maxIndex = findIndexOfMinimum(a, i, a.length);
             if (a[i] != a[maxIndex]) {
                 swap(a, i, maxIndex);
             }
         }
-        return a;
     }
 
-    public static int[] quickSort(int[] a) {
+    private static void quickSort(int[] a) {
         quickSortHelper(a, 0, a.length - 1);
-        return a;
     }
 
     private static void quickSortHelper(int[] a, int start, int end) {
@@ -66,30 +79,38 @@ public class SortSolution {
 
     }
 
-    public static boolean validate(int[] a) {
+    private static boolean validate(int[] a) {
         for (int i = 0;i < a.length - 1;i++) {
             if (a[i] > a[i + 1]) {
-                return false;
+                return true;
             }
         }
-        return true;
+        return false;
     }
 
     public static void main(String[] args) {
-        int[] a = {1, 3, 5 , 6 , 7, 2, 8};
-        selectionSort(a);
-        if (!validate(a)) {
+        int[] a = {8, 3, 2, 5, 9, 1, 6};
+        insertionSort(a);
+        if (validate(a)) {
             System.out.println("error");
         } else {
             System.out.println(Arrays.toString(a));
         }
 
-        int[] b = {12, 15, 6, 7, 4};
-        quickSort(b);
-        if (!validate(b)) {
+        int[] b = {1, 3, 5 , 6 , 7, 2, 8};
+        selectionSort(b);
+        if (validate(b)) {
             System.out.println("error");
         } else {
             System.out.println(Arrays.toString(b));
+        }
+
+        int[] c = {12, 15, 6, 7, 4};
+        quickSort(c);
+        if (validate(c)) {
+            System.out.println("error");
+        } else {
+            System.out.println(Arrays.toString(c));
         }
     }
 }
