1206163d2145be623ae220978c7b8f969c4f2c53
Yobol <m15152036579@163.com>
2019-03-07 00:10:01 +0800

    polish Num Of 1

--- src/me/yobol/yoalgo/bit/NumOf1.java
+++ src/me/yobol/yoalgo/bit/NumOf1.java
@@ -2,19 +2,27 @@ package me.yobol.yoalgo.bit;
 
 public class NumOf1 {
 
-    public int NumOf1(int n) {
+    public int NumOfOne1(int n) {
         int ctn = 0;
         while (n != 0) {
-            if ((n & 1) == 1) {
-                ctn += 1;
-            }
+            ctn += n & 1;
             n >>>= 1;
+        }
+        return ctn;
+    }
+
+    public int NumOfOne2(int n) {
+        int ctn = 0;
+        while (n != 0) {
+            // 抹除掉n最右边的1
+            n &= (n - 1);
+            ctn++;
         }
         return ctn;
     }
 
     public static void main(String[] args) {
         NumOf1 s = new NumOf1();
-        System.out.println(s.NumOf1(-31));
+        System.out.println(s.NumOfOne1(-31));
     }
 }
